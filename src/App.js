6f02a6c0be6f1dcafb84e99434266f985a3c1c69
paxman234 <sav-fernandes@outlook.com>
2024-12-04 12:23:31 +0000

    initial project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Root from './components/root';
+
+// Add react-router-dom imports
+
+// create router with JSX Route elements
+const appRouter = "REPLACE ME";
+
+function App() {
+  return (
+    <HomePage />
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const { worker } = require('./mocks/browser');
+worker.start();
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
